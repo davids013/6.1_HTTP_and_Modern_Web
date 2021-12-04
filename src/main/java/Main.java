5b0757ecb93f39ccf60033ff4a3e32ba22d31235
Vitaly Shvedko <davids013013@gmail.com>
2021-12-05 00:20:58 +0300

    Added: POST handler and post.html

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,6 +31,21 @@ public class Main {
         server.addHandler("POST", "/messages", (request, responseStream) -> {
             // TODO: handlers code
             System.out.println("Header for \"POST /messages\" called");
+            try {
+                final Path filePath = Path.of(".", "public", "/post.html");
+                final String mimeType = Files.probeContentType(filePath);
+                final long length = Files.size(filePath);
+                final String response = "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n";
+                responseStream.write(response.getBytes());
+                Files.copy(filePath, responseStream);
+                responseStream.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
 
         server.listen(9999);
