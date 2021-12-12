48b2f28c2127ee19f81f2b7f16647c9d1f0642a5
Vitaly Shvedko <davids013013@gmail.com>
2021-12-12 22:35:40 +0300

    Add query support

--- src/main/java/server/Request.java
+++ src/main/java/server/Request.java
@@ -1,5 +1,13 @@
 package server;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,57 +17,38 @@ public class Request {
     private static final String LINE_SEPARATOR = "\r\n";
     private static final String HEADER_SEPARATOR = ": ";
     private static final String METHOD_SEPARATOR = " ";
+    private static final String EMPTY_PART_SYMBOL = "ND";
     private final String method;
     private final String requestLine;
     private final String path;
     private final String httpVersion;
     private final Map<String, String> headers;
     private final String body;
+    private final List<NameValuePair> queryParams;
 
-    public Request(String requestLine, Map<String, String> headers, String body) {
+    private Request(String requestLine, Map<String, String> headers, String body) {
         this.requestLine = requestLine;
+        this.headers = headers == null ? new HashMap<>() : headers;
+        this.body = body;
         final String[] parts = requestLine.split(METHOD_SEPARATOR);
         if (parts.length == 3) {
             method = parts[0];
-            path = parts[1];
+            if (!parts[1].contains("?")) {
+                path = parts[1];
+                queryParams = new ArrayList<>();
+            } else {
+                String temp = parts[1];
+                path = temp.substring(0, temp.indexOf("?"));
+                temp = temp.substring(temp.indexOf("?") + 1);
+                queryParams = parseQueryParams(temp);
+            }
             httpVersion = parts[2];
         } else {
-            this.method = null;
-            this.path = null;
-            httpVersion = "HTTP/1.1";
+            this.method = EMPTY_PART_SYMBOL;
+            this.path = EMPTY_PART_SYMBOL;
+            httpVersion = EMPTY_PART_SYMBOL;
+            queryParams = new ArrayList<>();
         }
-        this.headers = headers;
-        this.body = body;
-    }
-
-    public Request(String method, String path, Map<String, String> headers, String body) {
-        this.method = method;
-        this.path = path;
-        httpVersion = "HTTP/1.1";
-        requestLine = method + METHOD_SEPARATOR + path + METHOD_SEPARATOR + httpVersion;
-        this.headers = headers;
-        this.body = body;
-    }
-
-    public Request(String requestStr) {
-        if (requestStr.contains(METHOD_SEPARATOR)) {
-            final int requestLastIndex = (!requestStr.contains(LINE_SEPARATOR))
-                    ? requestStr.length()
-                    : requestStr.indexOf(LINE_SEPARATOR);
-            requestLine = requestStr.substring(0, requestLastIndex);
-        } else requestLine = null;
-        final String[] parts = requestLine.split(METHOD_SEPARATOR);
-        if (parts.length == 3) {
-            this.method = parts[0];
-            this.path = parts[1];
-            this.httpVersion = parts[2];
-        } else {
-            method = null;
-            path = null;
-            httpVersion = null;
-        }
-        this.headers = null;
-        this.body = null;
     }
 
     public String getMethod() {
@@ -86,44 +75,76 @@ public class Request {
         return httpVersion;
     }
 
-    public static Request parseRequest(String requestStr) {
-        if (requestStr.contains(METHOD_SEPARATOR)) {
-            final String[] lines = requestStr.split(LINE_SEPARATOR);
-            final String requestLine = lines[0];
-            final List<String> keys = new ArrayList<>();
-            final List<String> values = new ArrayList<>();
-            if (requestStr.length() > requestLine.length() + 1) {
-                for (int i = 1; i < lines.length; i++) {
-                    if (lines[i].contains(HEADER_SEPARATOR)) {
-                        keys.add(lines[i].substring(0, lines[i].indexOf(HEADER_SEPARATOR)).trim());
-                        values.add(lines[i].substring(lines[i].indexOf(HEADER_SEPARATOR) + 1).trim());
-                    }
-                }
-            }
-            final Map<String, String> headers = new HashMap<>();
-            for (int i = 0; i < keys.size(); i++) {
-                headers.put(keys.get(i), values.get(i));
-            }
-            int lastHeaderIndex = -1;
-            for (int i = 0; i < lines.length; i++) {
-                if (i != 0 && !lines[i].contains(HEADER_SEPARATOR)) {
-                    lastHeaderIndex = i;
-                    break;
-                }
-            }
-            final String body;
-            if (lastHeaderIndex == -1) {
-                body = null;
-            } else {
-                final StringBuilder sb = new StringBuilder();
-                for (int i = lastHeaderIndex; i < lines.length; i++) {
-                    sb.append(lines[i]);
-                    if (i < lines.length - 1) sb.append(LINE_SEPARATOR);
-                }
-                body = sb.toString();
-            }
-            return new Request(requestLine, headers, body);
-        } else
-            return null;
+    public List<NameValuePair> getQueryParams() {
+        return new ArrayList<>(queryParams);
+    }
+
+    public static Request fromInputStream(InputStream inputStream) throws IOException {
+        final BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+        final String requestLine = in.readLine();
+        System.out.println(">> " + requestLine);
+        if (requestLine == null)
+            throw new IOException("Null requested!");
+        final String[] parts = requestLine.split(METHOD_SEPARATOR);
+        if (parts.length != 3) throw new IOException("Invalid request!");
+        final Map<String, String> headers = new HashMap<>();
+        String line;
+        while (!(line = in.readLine()).isEmpty()) {
+            final int index = line.indexOf(": ");
+            final String key = line.substring(0, index);
+            final String value = line.substring(index + 2);
+            headers.put(key, value);
+        }
+        return new Request(requestLine, headers, "");
+    }
+
+//    public static Request parseRequest(String requestStr) throws IOException {
+//        System.out.println(">> " + requestStr);
+//        if (requestStr == null)
+//            throw new IOException("Null requested!");
+//        final String[] lines = requestStr.split(LINE_SEPARATOR);
+//        final String requestLine = lines[0];
+//        if (requestStr.contains(METHOD_SEPARATOR)) {
+//            final List<String> keys = new ArrayList<>();
+//            final List<String> values = new ArrayList<>();
+//            if (requestStr.length() > requestLine.length() + 1) {
+//                for (int i = 1; i < lines.length; i++) {
+//                    if (lines[i].contains(HEADER_SEPARATOR)) {
+//                        keys.add(lines[i].substring(0, lines[i].indexOf(HEADER_SEPARATOR)).trim());
+//                        values.add(lines[i].substring(lines[i].indexOf(HEADER_SEPARATOR) + 1).trim());
+//                    }
+//                }
+//            }
+//            final Map<String, String> headers = new HashMap<>();
+//            for (int i = 0; i < keys.size(); i++) {
+//                headers.put(keys.get(i), values.get(i));
+//            }
+//            int lastHeaderIndex = -1;
+//            for (int i = 0; i < lines.length; i++) {
+//                if (i != 0 && !lines[i].contains(HEADER_SEPARATOR)) {
+//                    lastHeaderIndex = i;
+//                    break;
+//                }
+//            }
+//            final String body;
+//            if (lastHeaderIndex == -1) {
+//                body = "";
+//            } else {
+//                final StringBuilder sb = new StringBuilder();
+//                for (int i = lastHeaderIndex; i < lines.length; i++) {
+//                    sb.append(lines[i]);
+//                    if (i < lines.length - 1) sb.append(LINE_SEPARATOR);
+//                }
+//                body = sb.toString();
+//            }
+//            return new Request(requestLine, headers, body);
+//        } else
+//            return null;
+//    }
+
+    public static List<NameValuePair> parseQueryParams(String query) {
+        if (query.contains("?"))
+            query = query.substring(query.indexOf("?") + 1);
+        return URLEncodedUtils.parse(query, StandardCharsets.UTF_8);
     }
 }
